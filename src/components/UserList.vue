4044db9ef5586f833bc289e1a07b38ade022a94a
Harsha072 <20100308@mail.wit.ie>
2022-12-25 12:29:55 +0000

    new commit to userlist

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -2,6 +2,7 @@
   <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
     <template v-slot:top>
       <v-toolbar flat>
+
         <v-toolbar-title>{{ title }}</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
