855089fc2117190a108733a8a996dd9ba569af00
Harsha072 <20100308@mail.wit.ie>
2022-12-28 19:32:02 +0000

    new package-lock json12 express

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const URL ='https://healthtrackerrest-production-f63f.up.railway.app'
 
 app.use(express.static(__dirname + "/dist/"));
 
-app.use("/api",proxy(URL))
+app.use("/",proxy(URL))
 
 app.get(/.*/, function(req, res) {
   res.sendFile(__dirname + "/dist/index.html");
