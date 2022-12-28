936fba617a2af849d99bf89d6e2c3a464d5a4013
Harsha072 <20100308@mail.wit.ie>
2022-12-28 18:57:41 +0000

    new package-lock json9 express

--- src/http-common.js
+++ src/http-common.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:7000/api",
-  baseURL: 'https://healthtrackerrest-production-f63f.up.railway.app/api',
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-type": "application/json"
-  }
+  // baseURL: 'https://healthtrackerrest-production-f63f.up.railway.app/api',
+  // headers: {
+  //   "Access-Control-Allow-Origin": "*",
+  //   "Content-type": "application/json"
+  // }
 });
