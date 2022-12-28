1755d072558dde19a08cb18ce7331e6e089664f7
Harsha072 <20100308@mail.wit.ie>
2022-12-28 17:55:32 +0000

    new package-lock json4 express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const proxy = require("express-http-proxy")
 const port = process.env.PORT || 8080;
 const app = express();
-
+const URL ='https://healthtrackerrest-production-f63f.up.railway.app'
+app.use("/",proxy(URL))
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
   res.sendfile(__dirname + "/dist/index.html");
