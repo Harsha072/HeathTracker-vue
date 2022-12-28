85d6bffeedead606583c21b911a7abe09dfbd3c4
Harsha072 <20100308@mail.wit.ie>
2022-12-28 18:50:13 +0000

    new package-lock json8 express

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + "/dist/"));
 app.use("/",proxy(URL))
 
 app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
+  res.sendFile(__dirname + "/dist/index.html");
 });
 app.listen(port);
 
--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,7 @@ export default axios.create({
   // baseURL: "http://localhost:7000/api",
   baseURL: 'https://healthtrackerrest-production-f63f.up.railway.app/api',
   headers: {
-
+    "Access-Control-Allow-Origin": "*",
     "Content-type": "application/json"
   }
 });
